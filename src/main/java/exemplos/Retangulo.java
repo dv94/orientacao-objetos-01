63763b01a948b4b5fd581460e8dd94b26d67b7a9
dv94 <davi_66henrique@hotmail.com>
2020-02-17 21:36:24 -0300

    Calculo de Retangulo

---
+++ src/main/java/exemplos/Retangulo.java
@@ -0,0 +1,14 @@
+package exemplos;
+
+public class Retangulo {
+    public Double base;
+    public Double altura;
+
+    public Double calcularArea(){
+        return base * altura;
+
+        public Double calcularPerimentro(){
+            return 2 * (base + altura)
+        }
+    }
+}
