fb9cbf3e532524a9adc3b6d9bcbc1aa6904dff0f
Davi Henrique Silva de Oliveira <davoliveira@gertec.local>
2020-02-21 12:22:57 -0300

    Criação de Teste de Correntista

---
+++ src/test/java/exercicios/CorrentistaTeste.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CorrentistaTeste {
+
+    @Test
+    public void criarObejetoCorrentista(){
+        Correntista c1 = new Correntista();
+        assertNotNull(c1);
+    }
+    @Test
+    public void criarAtributos(){
+        Correntista c1 = new Correntista();
+        c1.codigo = "1";
+        c1.nome = "Juan";
+        c1.telefone = "11-111111111";
+        c1.email = "juan@juan.com";
+        assertNotNull(c1.codigo);
+        assertNotNull(c1.nome);
+        assertNotNull(c1.telefone);
+        assertNotNull(c1.email);
+
+    }
+
+}
